69e5eaa788eeff1806bbea9bd4702b4ba6a39078
Alri42 <alri.naudi@gmail.com>
2019-10-18 17:09:44 +0200

    Premier début

---
+++ app/src/main/java/com/example/plagesvivantes/Protocole.java
@@ -0,0 +1,14 @@
+package com.example.plagesvivantes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Protocole extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_protocole);
+    }
+}
